9acdb09513d822c0b1d080d96485bf873633a2b1
cenk.eralp <cenk.eralp@siemens.com>
2022-06-12 02:18:18 +0300

    A

--- medmnist_supervised.py
+++ medmnist_supervised.py
@@ -10,8 +10,76 @@ import copy
 
 import medmnist
 from medmnist import INFO, Evaluator
+
 import os
-import torchvision.datasets
+from torchvision.utils import save_image
+import matplotlib.image
+
+data_flag = 'pathmnist'
+# data_flag = 'breastmnist'
+download = True
+
+NUM_EPOCHS = 100
+BATCH_SIZE = 64
+PATIENCE = 10
+lr = 0.001
+weight_decay = 0.001
+number_of_res_blocks = 5
+
+info = INFO[data_flag]
+task = info['task']
+n_channels = info['n_channels']
+n_classes = len(info['label'])
+
+DataClass = getattr(medmnist, info['python_class'])
+task
+
+workdir = os.getcwd() + "/data-local/images/"
+os.makedirs(workdir, exist_ok=True)
+
+train_dataset = DataClass(split='train', download=download)
+val_dataset = DataClass(split='val', download=download)
+test_dataset = DataClass(split='test', download=download)
+
+
+train_dir = os.path.abspath(os.path.join(workdir, 'train'))
+val_dir = os.path.abspath(os.path.join(workdir, 'val'))
+test_dir = os.path.abspath(os.path.join(workdir, 'test'))
+
+number_of_labeled = 1000
+labeled_indeces = np.random.uniform(low=0, high=len(train_dataset)-1, size=(number_of_labeled,)).astype('int')
+unlabeled_indeces = np.delete(range(len(train_dataset)), labeled_indeces)
+
+label_names = info['label']
+print(label_names)
+
+def write_image(target_dir, index, x, y):
+    subdir = os.path.join(target_dir, str(y))
+    name = "{}_{}.png".format(index, y)
+    os.makedirs(subdir, exist_ok=True)
+    t = transforms.ToTensor()
+    save_image(t(x), os.path.join(subdir, name))
+    return
+
+file_object = open(os.path.join(workdir, "labels.txt"), 'a')
+
+for i in range(len(train_dataset)):
+    x, y = train_dataset[i][0], train_dataset[i][1]
+    
+    write_image(train_dir, i, x, y)
+    if i in labeled_indeces:
+        file_object.write('{}_{}.png {}\n'.format(i, y, y))
+
+file_object.close()
+
+for i in range(len(val_dataset)):
+    x, y = val_dataset[i][0], val_dataset[i][1][0]
+    write_image(val_dir, i, x, y)
+
+for i in range(len(test_dataset)):
+    x, y = test_dataset[i][0], test_dataset[i][1]
+    write_image(test_dir, i, x, y)    
+    
 
 workdir = os.getcwd() + "/data-local/images/"
 os.makedirs(workdir, exist_ok=True)
@@ -56,10 +124,10 @@ test_dir = os.path.abspath(os.path.join(workdir, 'test'))
 
 print("---"*30, train_dir)
 
-train_dataset = torchvision.datasets.ImageFolder(train_dir, data_transform)
+#train_dataset = torchvision.datasets.ImageFolder(train_dir, data_transform)
 train_dataset = train_dataset[labeled_indeces]
-val_dataset = torchvision.datasets.ImageFolder(val_dir, data_transform)
-test_dataset = torchvision.datasets.ImageFolder(test_dir, data_transform)
+#val_dataset = torchvision.datasets.ImageFolder(val_dir, data_transform)
+#test_dataset = torchvision.datasets.ImageFolder(test_dir, data_transform)
 
 # encapsulate data into dataloader form
 train_loader = data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
